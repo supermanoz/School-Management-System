7114d7a88f06e018e8b4fe5b27ddd84a6821c605
Ganesh Khapung <gnsubba@gmail.com>
2023-06-27 13:45:23 +0545

    init: Initialize academic module

---
+++ academic/src/test/java/com/example/academic/AcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

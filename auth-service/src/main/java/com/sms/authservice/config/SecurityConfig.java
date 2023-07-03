19fb77a43b18707c0f3e8ca0b4a04d7abcb8afe5
Manoj Basnet <basnetm02@gmail.com>
2023-07-03 16:38:58 +0545

    Jwt Bearer implemented for api requests

--- auth-service/src/main/java/com/sms/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/sms/authservice/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.sms.authservice.config;
 
+import com.sms.authservice.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,6 +22,7 @@ public class SecurityConfig {
                 .antMatchers("/api/auth/getToken").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
-                .and().build();
+                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
---
+++ auth-service/src/main/java/com/sms/authservice/filter/JwtFilter.java
@@ -0,0 +1,42 @@
+package com.sms.authservice.filter;
+
+import com.sms.authservice.userdetail.SmsUserDetailsService;
+import com.sms.authservice.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private SmsUserDetailsService smsUserDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader=request.getHeader("Authorization");
+        if(authHeader!=null && authHeader.startsWith("Bearer ")){
+            String jwt=authHeader.substring(7);
+            String username=jwtUtil.extractUsername(jwt);
+            if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+                UserDetails userDetails=this.smsUserDetailsService.loadUserByUsername(username);
+                if(jwtUtil.validateToken(jwt,userDetails)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}

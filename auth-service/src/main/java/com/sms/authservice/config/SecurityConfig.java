e092b232ffff4375724e9ae36511da668578ead0
Manoj Basnet <basnetm02@gmail.com>
2023-07-03 17:24:36 +0545

    auth service complete

--- auth-service/src/main/java/com/sms/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/sms/authservice/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -28,6 +29,7 @@ public class SecurityConfig {
                 .antMatchers("/api/auth/getToken").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new RequestSourceFilter(AUTH_TOKEN_HEADER_NAME,AUTH_TOKEN), UsernamePasswordAuthenticationFilter.class)
                 .build();
--- auth-service/src/main/java/com/sms/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/sms/authservice/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.sms.authservice.controller;
 import com.sms.authservice.dto.AuthRequest;
 import com.sms.authservice.dto.AuthResponse;
 import com.sms.authservice.service.AuthService;
+import com.sms.response.SmsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,12 @@ public class AuthController {
     }
 
     @GetMapping("/getToken")
-    public AuthResponse getToken(@RequestBody AuthRequest user) throws Exception {
-        return authService.getToken(user);
+    public ResponseEntity<SmsResponse> getToken(@RequestBody AuthRequest user) throws Exception {
+        return ResponseEntity.ok().body(new SmsResponse("JWT Token",true,authService.getToken(user)));
+    }
+
+    @GetMapping("/authenticate")
+    public ResponseEntity<SmsResponse> authenticate(){
+        return ResponseEntity.ok().body(new SmsResponse("Authenticated",true,true));
     }
 }

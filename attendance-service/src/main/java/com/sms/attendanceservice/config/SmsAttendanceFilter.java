d10d768b4b00a7d276e960f5f4dd39395d99a1ac
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2023-07-06 11:56:15 +0545

    authencation filter is added in attendanceService

--- attendance-service/src/main/java/com/sms/attendanceservice/config/SmsAttendanceFilter.java
+++ attendance-service/src/main/java/com/sms/attendanceservice/config/SmsAttendanceFilter.java
@@ -1,2 +1,23 @@
-package com.sms.attendanceservice.config;public class SmsAttendanceFilter {
+package com.sms.attendanceservice.config;
+
+import com.sms.filter.RequestSourceFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SmsAttendanceFilter {
+
+    @Value("${sms.auth.token.header.name}")
+    private String AUTH_TOKEN_HEADER_NAME;
+    @Value("${sms.auth.token.value}")
+    private String AUTH_TOKEN;
+    @Bean
+    public FilterRegistrationBean<RequestSourceFilter> requestSourceFilterFilterRegistrationBean(){
+        FilterRegistrationBean<RequestSourceFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestSourceFilter(AUTH_TOKEN_HEADER_NAME,AUTH_TOKEN));
+        registrationBean.addUrlPatterns("/api/attendances/*");
+        return registrationBean;
+    }
 }

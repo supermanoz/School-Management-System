8ab958ec688f627198548b2b2379c4737ee3e7a3
Manoj Basnet <basnetm02@gmail.com>
2023-07-07 16:48:55 +0545

    registered course api in gateway and swagger docs

--- course-service/src/main/java/com/sms/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/com/sms/courseservice/CourseServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient

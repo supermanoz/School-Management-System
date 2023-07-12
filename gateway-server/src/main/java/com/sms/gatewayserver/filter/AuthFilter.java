4907890ab52d9858294aa946460885efd5a44fd9
Manoj Basnet <basnetm02@gmail.com>
2023-07-12 17:04:18 +0545

    header automatically attached to requests when going through gateway

--- gateway-server/src/main/java/com/sms/gatewayserver/filter/AuthFilter.java
+++ gateway-server/src/main/java/com/sms/gatewayserver/filter/AuthFilter.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -61,11 +62,18 @@ public class AuthFilter {
                         header.set("Authorization", exchange.getRequest().getHeaders().getFirst("Authorization"));
                     })
                     .exchange()
-                    .flatMap(response->response.statusCode().value()==200?chain.filter(exchange):setModifiedResponse(exchange));
+                    .flatMap(response->response.statusCode().value()==200?setAuthorizedResponse(exchange,chain):setUnauthorizedResponse(exchange));
         };
     }
 
-    private Mono<Void> setModifiedResponse(ServerWebExchange exchange){
+    private Mono<Void> setAuthorizedResponse(ServerWebExchange exchange,GatewayFilterChain chain){
+        HttpHeaders headers=HttpHeaders.writableHttpHeaders(exchange.getRequest().getHeaders());
+        headers.set(AUTH_TOKEN_HEADER_NAME,AUTH_TOKEN_HEADER_VALUE);
+        LOGGER.trace("Setting api auth header to authorized request");
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> setUnauthorizedResponse(ServerWebExchange exchange){
         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
         LOGGER.trace("Setting response as unauthorized");
         return exchange.getResponse().setComplete();
